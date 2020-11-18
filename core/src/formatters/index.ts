0352889e02764ae225bfc89922bca4d42246ea6d
szhshp <24031shp@sina.com>
2020-11-18 17:22:56 +0800

    Update

--- core/src/formatters/index.ts
+++ core/src/formatters/index.ts
@@ -1,4 +1,4 @@
-import { Language } from '@src/formatters/types'
+import { Language } from '@src/types'
 
 /**
  * @name removeBlankLinesAtTheEnd
---
+++ core/src/index.d.ts
@@ -0,0 +1,1 @@
+declare module 'bisheng-formatter-core';
