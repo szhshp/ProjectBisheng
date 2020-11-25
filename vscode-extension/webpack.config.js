85b59f8f309154d93baed9f898f8b9336c19162e
szh-hp-laptop <24031shp@sina.com>
2020-11-25 23:20:19 +0800

    Update

--- vscode-extension/webpack.config.js
+++ vscode-extension/webpack.config.js
@@ -34,6 +34,18 @@ const config = {
             loader: 'ts-loader'
           }
         ]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
       }
     ]
   }
