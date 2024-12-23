8066882f12cc983f86c37b1a342ad0eadd7b0216
Szhshp <24031shp@sina.com>
2024-12-23 23:16:59 +0800

    feat: update package version to 0.2.14 and add CLI support

--- core/src/bin/cli.ts
+++ core/src/bin/cli.ts
@@ -28,7 +28,8 @@ const argv = yargs(process.argv.slice(2))
   })
   .option("addSpacesBetweenChineseCharAndAlphabeticalChar", {
     type: "boolean",
-    description: "Add spaces between Chinese characters and alphabetical characters",
+    description:
+      "Add spaces between Chinese characters and alphabetical characters",
   })
   .option("halfWidthCharsAndFollowingSpaces", {
     type: "boolean",
@@ -42,27 +43,27 @@ const argv = yargs(process.argv.slice(2))
     type: "number",
     choices: [3, 6],
     description: "Choose ellipsis count (3 or 6)",
-  })
-  .argv;
+  }).argv;
 
 // Get file paths from arguments
 const files = argv._;
 
 // Create BishengMainConfig based on arguments
-const config =
-
-{
+const config = {
   mainFeature: {
     markdownLinksInFullWidth: argv.markdownLinksInFullWidth || true,
     boldTextBlock: argv.boldTextBlock || true,
     blankLines: argv.blankLines || true,
     duplicatedPunctuations: argv.duplicatedPunctuations || true,
-    fullWidthCharsAndFollowingSpaces: argv.fullWidthCharsAndFollowingSpaces || true,
-    addSpacesBetweenChineseCharAndAlphabeticalChar: argv.addSpacesBetweenChineseCharAndAlphabeticalChar || true,
-    halfWidthCharsAndFollowingSpaces: argv.halfWidthCharsAndFollowingSpaces || true,
+    fullWidthCharsAndFollowingSpaces:
+      argv.fullWidthCharsAndFollowingSpaces || true,
+    addSpacesBetweenChineseCharAndAlphabeticalChar:
+      argv.addSpacesBetweenChineseCharAndAlphabeticalChar || true,
+    halfWidthCharsAndFollowingSpaces:
+      argv.halfWidthCharsAndFollowingSpaces || true,
   },
   useSimpleQuotation: argv.useSimpleQuotation || true,
-  ellipsisCount: argv.ellipsisCount || 3,
+  ellipsisCount: (argv.ellipsisCount as 6 | 3) || 3,
 };
 
 // If no files are provided, remind the user
@@ -76,7 +77,6 @@ if (files.length === 0) {
 
       // Format the content using the configuration
       const formattedContent = format(content, config);
-      console.log('formattedContent: ', formattedContent === content);
 
       // Write the formatted content back to the file
       fs.writeFileSync(file, formattedContent, { encoding: "utf-8" });
@@ -84,7 +84,7 @@ if (files.length === 0) {
       // Log success message
       console.log(`File formatted successfully: ${file}`);
     } catch (error) {
-      console.log('error: ', error);
+      console.log("error: ", error);
       // Handle any errors (e.g., file not found, permission issues)
       console.error(`Error formatting file: ${file}`, error);
     }
